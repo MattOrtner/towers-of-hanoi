0259b274fdab0dfc300ef486815b05092ca9eb10
W00ST <matthewortner@gmail.com>
2020-07-18 13:43:12 -0400

    fixed pop-up, added ring renders, connected slider

--- src/App.js
+++ src/App.js
@@ -3,36 +3,57 @@ import './App.css';
 import TowersOfHanoi from './towers-of-hanoi'
 
 const towersOfHanoi = new TowersOfHanoi()
-let firstSelection
+
+const firstSelection = {}
 
 function App() {
   const [board, setBoard] = useState([...towersOfHanoi.board])
   const [isWin, setWin] = useState(false)
+  const [counter, setCounter] = useState(0)
+
+
 
   const selectTower = (index) => {
     return (event) => {
-      if (firstSelection === undefined) {
-        firstSelection = index
-        event.currentTarget.classList.add('selected')
+      if (firstSelection.index === undefined) {
+        firstSelection.index = index
+        firstSelection.element = event.currentTarget
+        firstSelection.element.classList.add('selected')
 
       } else {
 
-        if (towersOfHanoi.move(firstSelection, index)) {
+        if (towersOfHanoi.move(firstSelection.index, index)) {
           setBoard([...towersOfHanoi.board])
-          
+          firstSelection.element.classList.remove('selected')
+          setCounter(counter + 1)
           if (towersOfHanoi.checkWin()) {
             setWin(true)
           }
         }
-        firstSelection = undefined
-        event.currentTarget.classList.remove('selected')
-        
+        firstSelection.index = undefined
       }
     }
-
   }
-  const restart = () => {
+
+  const createNumRings = (event) => {
+    const ringsSelected = event.currentTarget.value
+    const numRings = parseInt(ringsSelected)
+
+    
+    const newArray = []
+    for (let i = 1; i <= numRings; i += 1) {
+      newArray.push(i)
+    }
+    
+    towersOfHanoi.board = [newArray, [], []]
+    setBoard([...towersOfHanoi.board])
+    
+  }
+    
+    const restart = () => {
+      
     setWin(false)
+    setCounter(0)
     towersOfHanoi.restart()
     setBoard([...towersOfHanoi.board])
   }
@@ -70,24 +91,30 @@ function App() {
   return (
     <div className="App">
       <h1 className='title'>Towers Of Hanoi</h1>
-      {isWin &&
-        <h1>YOU DID IT! YOU WON! GREAT JOB!</h1>
+      {!isWin && 
+      <h1 className='counter'>Number of moves: {counter}</h1>
       }
+      {isWin &&
+        <h1>YOU DID IT!      In {counter} moves!</h1>
+      }
+      <div>
+        <label> Number of rings
+          <input onChange={createNumRings} type='range' min='3' max='7' defaultValue='3' step='1'></input>
+        </label>
+      </div>
       <div className='border'>
         {renderTowers()}
       </div>
       <br />
-      {isWin &&
-        <button className='restartButton' onClick={restart}>Restart?</button>
-      }
+      <button className='restartButton' onClick={restart}>Restart?</button>
     </div>
   );
 }
 
 export default App;
 /**
- * adding some sort of indicator for ring selection
+ * fix winning towers number on instance of win function
  * adding posts for each tower to live on
- * adding a move counter
- * let the user choose the number of ring input type='range'
+ * have the restart function reset the whole-
+ * board w/ correct number of rings
  */
